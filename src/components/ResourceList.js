494f158a57908c58515375a18924f05a786348c6
mythLabs <amitbisht744@gmail.com>
2018-12-14 11:24:05 +0530

    use of useEffect as lifecycle

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-class ResourceList extends React.Component {
-    state = { resources: []}
+const ResourceList = ({ resource }) => {
+  //////    Without hooks in class based component  start  //////
+  //     state = { resources: []}
 
-   async componentDidMount() {
-       const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+  //    async componentDidMount() {
+  //        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
 
-        this.setState({resources: response.data})
-    }
+  //         this.setState({resources: response.data})
+  //     }
 
-    async componentDidUpdate(prevProps) {
-        if (prevProps.resource !== this.props.resource) { // pure hate
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+  //     async componentDidUpdate(prevProps) {
+  //         if (prevProps.resource !== this.props.resource) { //PURE HATE -> overcome by hooks
+  //             const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
 
-            this.setState({resources: response.data})
-        }
-        
-    }
+  //             this.setState({resources: response.data})
+  //         }
 
-    render() {
-        return<div>{this.state.resources.length}</div>
-    }
-}
+  //     }
+  //////    Without hooks in class based component  end   //////
+
+  /////   With hooks  //////
+
+  const [resources, setResources] = useState([]);
+
+  useEffect(
+    () => {
+      (async resource => {
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/${resource}`
+        );
+
+        setResources(response.data);
+      })(resource);  //useEffect cleanup requirement (for async) fulfillment intelligently
+    },
+    [resource]
+  );
+
+  return <div>{resources.length}</div>;
+};
 
 export default ResourceList;
