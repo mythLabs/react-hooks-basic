e27411cecefca49bb8c343dae48b8f2064fde2c5
mythLabs <amitbisht744@gmail.com>
2018-12-14 10:35:54 +0530

    Added useState hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
 
-class App extends React.Component{
-   state = { resource: 'posts'}
-
-    render() {
-        return (
-            <div>
-                <div>
-                    <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
-                    <button onClick={() => this.setState({resource: 'todos'})} >Todo</button>
-                </div>
-                {this.state.resource}
-            </div>
-        )
-    }
-   
-}
+const App = () => {
+    const [resource,setResource] = useState('posts') //Array destructuring
+  return (
+    <div className="ui container">
+      <div>
+        <button onClick={() => setResource('posts' )}>
+          Posts
+        </button>
+        <button onClick={() => setResource('todos')}>
+          Todo
+        </button>
+      </div>
+      {resource}
+    </div>
+  );
+};
 
 export default App;
